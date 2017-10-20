8652e5c6b3e86a44ca82268c99de75db3c9e9c7b
unknown <zhengw@cmfdev.cmfchina.com>
2017-10-20 16:43:17 +0800

    修正合约无法生成的bug

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,4 @@
 var ContractFactory = artifacts.require("./ContractFactory.sol");
-var CFToken =artifacts.require("./FCToken.sol");
-var CCToken =artifacts.require("./CCToken.sol");
 module.exports = function(deployer) {
     deployer.deploy(ContractFactory);
-    deployer.deploy(CFToken);
-    deployer.deploy(CCToken);
 };
--- migrations/3_deploy_contracts.js
+++ migrations/3_deploy_contracts.js
@@ -1,6 +1,5 @@
 var CCTG = artifacts.require("CCTokenGenerator");
 var FCTG =artifacts.require("FCTokenGenerator");
-var CCToken =artifacts.require("./CCToken.sol");
 module.exports = function(deployer) {
     deployer.deploy(CCTG);
     deployer.deploy(FCTG);
